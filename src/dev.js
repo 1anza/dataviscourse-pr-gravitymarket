21fd862dd4785de289557593f7491674421d1d6b
adam <adam@adams-MacBook-Pro.local>
2022-11-08 13:10:21 -0700

    added better dev tool checkboxes

--- src/dev.js
+++ src/dev.js
@@ -1,16 +1,38 @@
 // attaches dev elements
 
 function addDevTools(gas) {
+
+
+	let sectors = [
+		"Industrials",
+		"Health Care",
+		"Technology",
+		"Communication Services",
+		"Consumer Cyclical",
+		"Energy",
+		"Financials",
+		"Basic Materials",
+		"Real Estate",
+		"Consumer Staples",
+		"Utilities",
+	];
+
+
 	d3.select("div#playback-div")
 		.append("div")
-		.text("toggle grouping")
+		.text("toggle grouping:")
+		.selectAll("input")
+		.data(sectors)
+		.enter()
 		.append("input")
 		.attr("type", "checkbox")
 		.on("change", e => {
-			if(e.target.checked) {
-				gas.set_selectedSectors(new Set(["Health Care", "Industrials"]));
+			let sector = e.target.__data__;
+			if (e.target.checked) {
+				gas.selectedSectors.add(sector);
 			} else {
-				gas.set_selectedSectors(new Set([]));
+				gas.selectedSectors.delete(sector);
 			}
-		})
+			gas.set_selectedSectors(gas.selectedSectors);
+		});
 }
