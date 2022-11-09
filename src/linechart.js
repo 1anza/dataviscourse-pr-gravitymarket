c1a4d050be6d88992e0250732f4a35644b31545d
Adam Colton <atcolton@tutanota.com>
2022-11-09 12:08:01 -0700

    added line drawing for all sectors regardless of gas.selectedSectors

--- src/linechart.js
+++ src/linechart.js
@@ -3,7 +3,7 @@ class Linechart {
 		this.gas = gas;
 		this.svg = d3.select("svg#linechart-vis");
 		this.bounds = {
-			minX: 20,
+			minX: 50,
 			maxX: 500,
 			minY: 20,
 			maxY: 500,
@@ -14,6 +14,10 @@ class Linechart {
 		this.updateScaleY();
 		this.updateAxisX();
 		this.updateAxisY();
+
+		// The motherline should be the first value in the data
+		this.persistentLineDataIndex = 0;
+		this.updateLines()
 	}
 
 	/*
@@ -30,7 +34,7 @@ class Linechart {
 
 	updateScaleY() {
 		let domain = this.gas.yValueDataRange;
-		let range = [this.bounds.minY, this.bounds.maxY];
+		let range = [this.bounds.maxY, this.bounds.minY];
 		this.scaleY = d3.scaleLinear().domain(domain).range(range);
 	}
 
@@ -47,5 +51,23 @@ class Linechart {
 
 		let yAxis = d3.axisLeft(this.scaleY);
 		axisG.call(yAxis);
+	}
+
+	updateLines() {
+		let paths = this.svg.select("g#lines")
+			.selectAll("path")
+			.data(this.gas.data)
+		paths
+			.join("path")
+			.attr("stroke", d => this.gas.colorFunc(d.sector))
+			.classed("linechart-path", true)
+			.attr("d", d => {
+				//console.log(d);
+				return d3.line()
+				.x(d => this.scaleX(dateMinuteToDate(d.date, d.minute)))
+				.y(d => this.scaleY(d.marketClose))
+				(d.chart)
+			}
+			)
 	}
 }
