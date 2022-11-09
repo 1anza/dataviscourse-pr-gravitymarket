55a11826cba48d1f47cd7264a37b3056e238f327
Adam Colton <atcolton@tutanota.com>
2022-11-09 12:39:02 -0700

    added percentYValueRange to gas

--- src/util.js
+++ src/util.js
@@ -2,3 +2,12 @@ function dateMinuteToDate(date, minute) {
 	let datestring = `${date}T${minute}`;
 	return new Date(datestring);
 }
+
+function getPercChange(row, index, yValueName) {
+	return (
+		(row.chart[index][yValueName] /
+			row.chart[0][yValueName] -
+			1) *
+		100
+	);
+}
