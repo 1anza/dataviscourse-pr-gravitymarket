d5601e6195e6c758b759f4a7d9e2ac63739b3123
Courtney Holbrook <u1156298@eng.utah.edu>
2022-11-11 14:20:02 -0700

    WE CAN DRAW LINES ON THE OHLC!

--- src/ohlc.js
+++ src/ohlc.js
@@ -52,16 +52,48 @@ class Ohlc {
         axisG.call(yAxis);
     }
 
-    updateOhlcChart(selected) {
+    updateOhlcChart(company) {
 
-        let domain = this.gas.yValueDataRange;
+
+        console.log(company.chart);
+
+        let domain = d3.extent(company.chart, d => d[this.gas.yValueName]);
         let range = [this.bounds.maxY, this.bounds.minY];
         this.scaleY = d3.scaleLinear().domain(domain).range(range);
 
         this.updateYAxis();
 
 
+        let series = this.svg.selectAll('.ohlc-series').append('g').classed('ohlc-series', true);
+        series.selectAll("g.ohlc-bar")
+            .data(company.chart)
+            .join("g")
+            .classed("ohlc-bar", true);
 
+       
+
+        let bars = series.selectAll('.ohlc-bar')
+            .data(company.chart, function (d) {
+                return d.date;
+            });
+
+        bars.enter()
+            .append('open-close-line')
+            .classed('bar', true);
+
+        var lines = bars
+            .selectAll('.open-close-line')
+            .data(function (d) {
+                return [d];
+            });
+
+        lines.enter().append('line')
+            .attr("stroke", "black")
+            .attr("stroke-width", "1")
+            .attr("x1", d => this.scaleX(dateMinuteToDate(d.date, d.minute)))
+            .attr("y1", d => this.scaleY(d.open))
+            .attr("x2", d => this.scaleX(dateMinuteToDate(d.date, d.minute)))
+            .attr("y2", d => this.scaleY(d.close));
 
     }
 
